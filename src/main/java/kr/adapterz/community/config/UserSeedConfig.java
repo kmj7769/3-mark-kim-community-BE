80e6d297b2d3264ea2c6cd528b2eecb5038b55b5
kimminjun <rlaalswns7769@gmail.com>
2025-11-27 16:30:04 +0900

    fix: UserSeedConfig.java - dev profile에서만 실행되도록 수정

--- src/main/java/kr/adapterz/community/config/UserSeedConfig.java
+++ src/main/java/kr/adapterz/community/config/UserSeedConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+// @Profile({"dev", "prod"})
 @Configuration
 @Profile("dev")
 @RequiredArgsConstructor
