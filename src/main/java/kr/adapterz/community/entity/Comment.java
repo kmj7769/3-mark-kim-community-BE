fd77bddcb22e702880612122c6e1a95b38fe586d
kimminjun <rlaalswns7769@gmail.com>
2025-11-27 16:32:37 +0900

    fix: comment.java - columnDefinition TEXT로 명시

--- src/main/java/kr/adapterz/community/entity/Comment.java
+++ src/main/java/kr/adapterz/community/entity/Comment.java
@@ -21,8 +21,7 @@ public class Comment {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Lob
-    @Column(nullable = false)
+    @Column(nullable = false, columnDefinition = "TEXT")
     private String content;
 
     @CreatedDate
